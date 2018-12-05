a9ec038235b7efe4d482362df7a9bc7aa74ce8fa
Marcel Wagner <marcel.alex.wagner@gmail.com>
2018-12-05 10:19:07 +0100

    Added day 5 solution

---
+++ AdventOfCode Tests/Solutions/Day5Tests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Solutions.Tests {
+  [TestClass()]
+  public class Day5Tests {
+    [TestMethod()]
+    public void convertTest() {
+      string[] data = { "a", "b", "c" };
+
+      String converted = Day5.convert(data);
+
+      Assert.AreEqual("a", converted);
+    }
+
+
+    [TestMethod()]
+    public void polymerTest_aA() {
+      string data = "aA";
+      string changed = Day5.polymer(data).Item1;
+      Assert.AreEqual("", changed);
+    }
+    [TestMethod()]
+    public void polymerTest_Zz() {
+      string data = "Zz";
+      string changed = Day5.polymer(data).Item1;
+      Assert.AreEqual("", changed);
+    }
+    [TestMethod()]
+    public void polymerTest_abBA() {
+      string data = "abBA";
+      string changed = Day5.polymer(data).Item1;
+      Assert.AreEqual("aA", changed);
+    }
+    [TestMethod()]
+    public void polymerTest_abAB() {
+      string data = "abAB";
+      string changed = Day5.polymer(data).Item1;
+      Assert.AreEqual("abAB", changed);
+    }
+    [TestMethod()]
+    public void polymerTest_aabAAB() {
+      string data = "aabAAB";
+      string changed = Day5.polymer(data).Item1;
+      Assert.AreEqual("aabAAB", changed);
+    }
+
+
+    [TestMethod()]
+    public void firstProblemTest_Simple(){
+      string data = "dabAcCaCBAcCcaDA";
+      int changed = Day5.firstProblem(data);
+      Assert.AreEqual(10,changed);
+    }   
+    
+    
+    [TestMethod()]
+    public void firstProblemTest_Simple2(){
+      string data = "dbcCCBcCcD";
+      int changed = Day5.firstProblem(data);
+      Assert.AreEqual(6,changed);
+    }   
+    
+    
+    [TestMethod()]
+    public void firstProblemTest_Simple3(){
+      string data = "dabAaBAaDA";
+      int changed = Day5.firstProblem(data);
+      Assert.AreEqual(4,changed);
+    }   
+    
+    
+    
+    [TestMethod()]
+    public void secondProblemTest_Simple(){
+      string data = "dabAcCaCBAcCcaDA";
+      int changed = Day5.secondProblem(data);
+      Assert.AreEqual(4,changed);
+    }
+  }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using AdventOfCode.Utils;
-using Solver = AdventOfCode.Solutions.Day4;
+using Solver = AdventOfCode.Solutions.Day5;
 
 
 namespace AdventOfCode {
@@ -10,7 +10,7 @@ namespace AdventOfCode {
       string fileName = "";
 
       //fileName = args[0];
-      fileName = @"D:\Projects\C#\AdventOfCode\AdventOfCode\Data\day4.txt";
+      fileName = @"D:\Projects\C#\AdventOfCode\AdventOfCode\Data\day5.txt";
 
       //Reading data
       string[] data = ReadUtils.readDataFromFile(fileName);
