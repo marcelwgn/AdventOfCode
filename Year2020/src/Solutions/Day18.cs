89ff822e709978419c489a612f477061eca62c69
Marcel Wagner <marcel.alex.wagner@outlook.com>
2020-12-18 16:28:35 +0100

    Add day 18

---
+++ Year2020/src/Solutions/Day18.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace AdventOfCode.Year2020.Solutions
+{
+    public static class Day18
+    {
+
+        public static long CalculateSolution(string[] data, bool plusHasPrecedence)
+        {
+            var sum = 0L;
+            foreach (var item in data)
+            {
+                sum += long.Parse(ProcessString(item, plusHasPrecedence));
+            }
+            return sum;
+        }
+
+        public static string ProcessString(string expression, bool plusHasPrecedence)
+        {
+            while (expression.Split('(').Length > 1)
+            {
+                int closest = expression.Length - 1;
+                for (int i = expression.Length - 1; i >= 0; i--)
+                {
+                    if (expression[i] == ')')
+                    {
+                        closest = i;
+                    }
+                    else if (expression[i] == '(')
+                    {
+                        string group = expression.Substring(i + 1, closest - i - 1);
+                        string total = ProcessString(group, plusHasPrecedence);
+                        expression = expression.Replace($"({group})", total.ToString());
+                        break;
+                    }
+                }
+            }
+
+            if (plusHasPrecedence)
+            {
+                return CalculateExpressionPlusHasPrecedence(expression).ToString();
+            }
+            else
+            {
+                return CalculateExpressionNormal(expression).ToString();
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static long CalculateExpressionNormal(string expression)
+        {
+            long total = 0;
+            var lastOperand = -1;
+            var lastExpressionWasAdd = true;
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '+')
+                {
+                    ProcessOperandEnd(i);
+                    lastExpressionWasAdd = true;
+                }
+                else if (expression[i] == '*')
+                {
+                    ProcessOperandEnd(i);
+                    lastExpressionWasAdd = false;
+                }
+            }
+
+            ProcessOperandEnd(expression.Length);
+
+            return total;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            void ProcessOperandEnd(int end)
+            {
+                if (lastExpressionWasAdd)
+                {
+                    total += long.Parse(expression[(lastOperand + 1)..end]);
+                }
+                else
+                {
+                    total *= long.Parse(expression[(lastOperand + 1)..end]);
+                }
+                lastOperand = end;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static long CalculateExpressionPlusHasPrecedence(string expression)
+        {
+            var parts = expression.Split(' ');
+            var numbers = new Stack<long>(parts.Length);
+            numbers.Push(long.Parse(parts[0]));
+            for (int i = 0; i < parts.Length - 2; i += 2)
+            {
+                if(parts[i + 1] == "+")
+                {
+                    var lastNumber = numbers.Pop();
+                    numbers.Push(lastNumber + long.Parse(parts[i + 2]));
+                }
+                else
+                {
+                    numbers.Push(long.Parse(parts[i+2]));
+                }
+            }
+            var result = 1L;
+            foreach(var item in numbers)
+            {
+                result *= item;
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Year2020/tests/Solutions/Day18Tests.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Specialized;
+using AdventOfCode.Year2020.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Year2020.Tests.Solutions
+{
+    [TestClass]
+    public class Day18Tests
+    {
+
+        [DataTestMethod]
+        [DataRow("1 + 2 * 3 + 4 * 5 + 6", "71")]
+        [DataRow("2 * 3 + (4 * 5)", "26")]
+        [DataRow("1 + (2 * 3) + (4 * 11)", "51")]
+        [DataRow("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", "12240")]
+        public void VerifyTransformNormal(string data, string expected)
+        {
+            Assert.AreEqual(expected, Day18.ProcessString(data, false));
+        }
+
+        [DataTestMethod]
+        [DataRow("1 + 2 * 3 + 4 * 5 + 6", "231")]
+        [DataRow("2 * 3 + (4 * 5)", "46")]
+        [DataRow("1 + (2 * 3) + (4 * 11)", "51")]
+        [DataRow("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", "669060")]
+        public void VerifyTransformPlusHasPrecedence(string data, string expected)
+        {
+            Assert.AreEqual(expected, Day18.ProcessString(data, true));
+        }
+
+        [TestMethod]
+        public void VerifyFirstProblem()
+        {
+            var data = new string[]
+            {
+                "1 + 2 * 3 + 4 * 5 + 6",
+                "2 * 3 + (4 * 5)",
+                "1 + (2 * 3) + (4 * 11)",
+                "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))",
+            };
+
+            Assert.AreEqual(12388L, Day18.CalculateSolution(data, false));
+        }
+
+        [TestMethod]
+        public void VerifySecondProblem()
+        {
+            var data = new string[]
+            {
+                "1 + 2 * 3 + 4 * 5 + 6",
+                "2 * 3 + (4 * 5)",
+                "1 + (2 * 3) + (4 * 11)",
+                "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))",
+            };
+
+            Assert.AreEqual(669388L, Day18.CalculateSolution(data, true));
+        }
+    }
+}
