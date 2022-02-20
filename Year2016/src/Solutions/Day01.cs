eb40ec249e38e54a6358f9288854eeb7dbca919a
Marcel Wagner <marcel.alex.wagner@outlook.com>
2022-02-20 12:50:44 +0100

    Add day 1, 2016

--- Year2016/src/Solutions/Day01.cs
+++ Year2016/src/Solutions/Day01.cs
@@ -6,7 +6,71 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode.Year2016.Solutions
 {
-    internal static class Day01
+    public static class Day01
     {
+        public static int FirstProblem(string[] data)
+        {
+            var instructions = data[0].Replace(" ", "").Split(",");
+            var lastPos = (0, 0, 0);
+            foreach (var instruction in instructions)
+            {
+                lastPos = GenerateNextPosition(instruction, lastPos).Last();
+            }
+            return Math.Abs(lastPos.Item1) + Math.Abs(lastPos.Item2);
+        }
+
+        public static int SecondProblem(string[] data)
+        {
+            var instructions = data[0].Replace(" ", "").Split(",");
+            var alreadyVisitedLocations = new HashSet<(int, int)>();
+            var lastPos = (0, 0, 0);
+
+            foreach (var instruction in instructions)
+            {
+                var visitedPositions = GenerateNextPosition(instruction, lastPos);
+                foreach (var pos in visitedPositions)
+                {
+                    if (alreadyVisitedLocations.Contains((pos.Item1, pos.Item2)))
+                    {
+                        return Math.Abs(pos.Item1) + Math.Abs(pos.Item2);
+                    }
+                    else
+                    {
+                        alreadyVisitedLocations.Add((pos.Item1, pos.Item2));
+                    }
+                }
+                lastPos = visitedPositions.Last();
+            }
+
+            return Math.Abs(lastPos.Item1) + Math.Abs(lastPos.Item2);
+        }
+
+
+        public static (int, int, int)[] GenerateNextPosition(string instruction, (int posX, int posY, int direction) lastPos)
+        {
+            lastPos.direction = (lastPos.direction + (instruction[0] == 'R' ? 1 : -1) + 4) % 4;
+            var increment = int.Parse(instruction[1..]);
+            var data = new (int, int, int)[increment];
+
+            for (int i = 1; i <= increment; i++)
+            {
+                switch (lastPos.direction)
+                {
+                    case 0:
+                        data[i - 1] = (lastPos.posX, lastPos.posY + i, lastPos.direction);
+                        break;
+                    case 1:
+                        data[i - 1] = (lastPos.posX + i, lastPos.posY, lastPos.direction);
+                        break;
+                    case 2:
+                        data[i - 1] = (lastPos.posX, lastPos.posY - i, lastPos.direction);
+                        break;
+                    case 3:
+                        data[i - 1] = (lastPos.posX - i, lastPos.posY, lastPos.direction);
+                        break;
+                }
+            }
+            return data;
+        }
     }
 }
--- Year2016/tests/Solutions/Day01Tests.cs
+++ Year2016/tests/Solutions/Day01Tests.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using AdventOfCode.Year2016.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AdventOfCode.Solutions
+namespace AdventOfCode.Year2016.Tests.Solutions
 {
-    internal class Day01Tests
+    [TestClass]
+    public class Day01Tests
     {
+
+        [TestMethod]
+        [DataRow("R2, L3",5)]
+        [DataRow("R2, R2, R2", 2)]
+        [DataRow("R5, L5, R5, R3", 12)]
+        public void FirstProblemTest(string data, int expectedValue)
+        {
+            Assert.AreEqual(expectedValue, Day01.FirstProblem(new string[] { data }));
+        }
+
+        [TestMethod]
+        public void SecondProblemTest()
+        {
+            Assert.AreEqual(4, Day01.SecondProblem(new string[] { "R8, R4, R4, R8" }));
+        }
     }
 }
