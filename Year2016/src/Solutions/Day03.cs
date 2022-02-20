4aca1dd24fc8151fd1cd68909202dea0c1e20cd9
Marcel Wagner <marcel.alex.wagner@outlook.com>
2022-02-20 13:54:40 +0100

    Add day 3

---
+++ Year2016/src/Solutions/Day03.cs
@@ -0,0 +1,41 @@
+﻿using AdventOfCode.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Year2016.Solutions
+{
+    public static class Day03
+    {
+        public static int[][] Convert(string[] data) => data.Select(x => x.Split(" ").Where(y => y != "").ToArray().ToIntArray()).ToArray();
+
+        public static int FirstProblem(int[][] data)
+        {
+            return data.Where(x => IsValidTriangle(x)).Count();
+        }
+
+        public static int SecondProblem(int[][] data)
+        {
+            int validCount = 0;
+            for (int i = 0; i < data.Length - 2; i += 3)
+            {
+                for (int j = 0; j < data[i].Length; j++)
+                {
+                    if (IsValidTriangle(new int[] { data[i][j], data[i + 1][j], data[i + 2][j] }))
+                    {
+                        validCount++;
+                    }
+                }
+            }
+            return validCount;
+        }
+
+        private static bool IsValidTriangle(int[] data)
+        {
+            return data[0] + data[1] > data[2] && data[1] + data[2] > data[0] && data[2] + data[0] > data[1];
+        }
+
+    }
+}
---
+++ Year2016/tests/Solutions/Day03Tests.cs
@@ -0,0 +1,58 @@
+﻿using AdventOfCode.Year2016.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Year2016.Tests.Solutions
+{
+    [TestClass]
+    public class Day03Tests
+    {
+
+        [TestMethod]
+        public void ConverterTest()
+        {
+            var data = new string[]
+            {
+                "1 2 3",
+                "     10 -1 20",
+                "     10 -1 20    ",
+                "     10   -1   20    ",
+            };
+
+            var converted = Day03.Convert(data);
+
+            CollectionAssert.AreEqual(new int[] {1,2,3} , converted[0]);
+            CollectionAssert.AreEqual(new int[] {10,-1,20}, converted[1]);
+            CollectionAssert.AreEqual(new int[] {10,-1,20}, converted[2]);
+            CollectionAssert.AreEqual(new int[] {10,-1,20}, converted[3]);
+        }
+
+        [TestMethod]
+        public void FirstProblemTest()
+        {
+            var data = new int[][]
+            {
+                new int[]{2,2,3},
+                new int[]{1000,2,3},
+                new int[]{1,2000,3},
+                new int[]{10,20,20},
+                new int[]{1,2,30000},
+            };
+            Assert.AreEqual(2, Day03.FirstProblem(data));
+        }
+
+        [TestMethod]
+        public void SecondProblemTest()
+        {
+            var data = new int[][]
+            {
+                new int[]{2,20,3},
+                new int[]{2,200,3},
+                new int[]{3,10,3},
+                new int[]{10,20,20},
+                new int[]{1,20,1},
+                new int[]{1,20,1},
+            };
+            Assert.AreEqual(3, Day03.SecondProblem(data));
+        }
+    }
+}
