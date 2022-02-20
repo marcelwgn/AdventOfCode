f79f62f0701d606492b8b23bde180491b4810aec
Marcel Wagner <marcel.alex.wagner@outlook.com>
2022-02-20 15:06:26 +0100

    Add day 4, 2016

--- Year2016/src/Program.cs
+++ Year2016/src/Program.cs
@@ -1,4 +1,4 @@
 ﻿using AdventOfCode.Common;
 using AdventOfCode.Year2016.Solutions;
 
-SolutionRunner.RunSolution(typeof(Day03));
+SolutionRunner.RunSolution(typeof(Day04));
---
+++ Year2016/src/Solutions/Day04.cs
@@ -0,0 +1,69 @@
+﻿using AdventOfCode.Common;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Year2016.Solutions
+{
+    public static class Day04
+    {
+        public static (string, int, string)[] Convert(string[] data)
+        {
+            return data.Select(entry =>
+            {
+                var roomEnd = entry.IndexOf('[');
+                var roomName = entry[0..(roomEnd - 4)];
+                var roomId = int.Parse(entry[(roomEnd - 3)..roomEnd]);
+                var checkKeys = entry[(roomEnd + 1)..(entry.Length - 1)];
+
+                return (roomName, roomId, checkKeys);
+            }).ToArray();
+        }
+
+        public static int FirstProblem((string, int, string)[] data)
+        {
+            return data.Select(x => Process(x)).Where(x => x.Item1).Sum(x => x.Item2);
+
+            static (bool, int) Process((string roomName, int roomId, string checkKeys) room)
+            {
+                var sorted = room.roomName.Replace("-", "").GroupBy(x => x).Select(x => (Count: x.Count(), Character: x.Key)).OrderByDescending(x => x.Count).ThenBy(x => x.Character).ToArray();
+
+                for (int i = 0; i < 5; i++)
+                {
+                    if (sorted[i].Character != room.checkKeys[i])
+                    {
+                        return (false, room.roomId);
+                    }
+                }
+                return (true, room.roomId);
+            }
+        }
+
+        public static int SecondProblem((string, int, string)[] data)
+        {
+            return data.Select(x => Process(x)).Where(x => x.Item1.Contains("pole")).Select(x => x.Item2).First();
+
+            static (string, int) Process((string roomName, int roomId, string checkKeys) room)
+            {
+                var newString = "";
+                foreach (var key in room.roomName)
+                {
+                    if (key != '-')
+                    {
+                        // lowercase a: 97
+                        newString += (char)(((key - 97 + room.roomId) % 26) + 97);
+                    }
+                    else
+                    {
+                        newString += '-';
+                    }
+                }
+                return (newString, room.roomId);
+            }
+        }
+    }
+}
