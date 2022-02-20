69846fe8019d840af7a1691bb7fa0d27536c9b3d
Marcel Wagner <marcel.alex.wagner@outlook.com>
2022-02-20 15:50:04 +0100

    Add day 5, 2016

---
+++ Year2016/src/Solutions/Day05.cs
@@ -0,0 +1,87 @@
+﻿using AdventOfCode.Common;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Year2016.Solutions
+{
+    public static class Day05
+    {
+        public static string FirstProblem(string[] data)
+        {
+            var result = "";
+            var md5 = System.Security.Cryptography.MD5.Create();
+
+            long index = 0;
+            while (result.Length < 8)
+            {
+                byte[] inputBytes = Encoding.ASCII.GetBytes(data[0] + index);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                if (hashBytes[0] == 0 && hashBytes[1] == 0)
+                {
+                    // Convert the byte array to hexadecimal string
+                    StringBuilder sb = new StringBuilder();
+                    for (int j = 0; j < hashBytes.Length; j++)
+                    {
+                        sb.Append(hashBytes[j].ToString("X2"));
+                    }
+                    if (sb.ToString().StartsWith("00000"))
+                    {
+                        result += sb.ToString()[5];
+                    }
+                }
+                index++;
+            }
+
+            return result.ToLower();
+        }
+
+        public static string SecondProblem(string[] data)
+        {
+            var md5 = System.Security.Cryptography.MD5.Create();
+
+            long index = 0;
+            char?[] password = new char?[] { null, null, null, null, null, null, null, null };
+            short fillCount = 0;
+            while (fillCount < 8)
+            {
+                byte[] inputBytes = Encoding.ASCII.GetBytes(data[0] + index);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                if (hashBytes[0] == 0 && hashBytes[1] == 0)
+                {
+                    // Convert the byte array to hexadecimal string
+                    StringBuilder sb = new StringBuilder();
+                    for (int j = 0; j < hashBytes.Length; j++)
+                    {
+                        sb.Append(hashBytes[j].ToString("X2"));
+                    }
+                    if (sb.ToString().StartsWith("00000"))
+                    {
+                        try
+                        {
+                            var insertIndex = int.Parse(sb.ToString()[5].ToString());
+                            if (insertIndex < 8 && password[insertIndex] == null)
+                            {
+                                fillCount++;
+                                password[insertIndex] = sb.ToString()[6];
+                            }
+                        } catch(Exception) { }
+                    }
+                }
+                index++;
+            }
+            var result = "";
+            foreach (var character in password)
+            {
+                result += character;
+            }
+            return result.ToLower();
+        }
+    }
+}
---
+++ Year2016/tests/Solutions/Day05Tests.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode.Year2016.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Year2016.Tests.Solutions
+{
+    [TestClass]
+    public class Day05Tests
+    {
+        [TestMethod]
+        public void FirstProblemTest()
+        {
+            Assert.AreEqual("18f47a30",Day05.FirstProblem(new string[] { "abc" }));
+        }
+
+        [TestMethod]
+        public void SecondProblemTest()
+        {
+            Assert.AreEqual("05ace8e3", Day05.SecondProblem(new string[] { "abc" }));
+        }
+    }
+}
