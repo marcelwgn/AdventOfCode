15f2dc3a8c9f3fc678edd476e033558cf7e62c70
Marcel Wagner <marcel.alex.wagner@outlook.com>
2022-02-23 21:17:36 +0100

    Add day 20, 2016

---
+++ Year2016/src/Solutions/Day20.cs
@@ -0,0 +1,58 @@
+﻿using AdventOfCode.Common;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Year2016.Solutions
+{
+    public static class Day20
+    {
+        public static (uint Start, uint Finish)[] Convert(string[] data) => data.Select(x => (uint.Parse(x.Split("-")[0]), uint.Parse(x.Split("-")[1]))).ToArray();
+
+        /// <summary>
+        /// This is the Josephus problem, we are using the algorithm for it.
+        /// </summary>
+        public static long FirstProblem((uint Start, uint Finish)[] data)
+        {
+            var intervals = GetUniqueIntervals(data.OrderBy(x => x.Start).ToArray());
+            return intervals[0].Finish + 1;
+        }
+
+        public static long SecondProblem((uint Start, uint Finish)[] data)
+        {
+            var intervals = GetUniqueIntervals(data.OrderBy(x => x.Start).ToArray());
+            long freeIPCount = 0;
+            for (int i = 0; i < intervals.Count - 1; i++)
+            {
+                freeIPCount += intervals[i + 1].Start - intervals[i].Finish - 1;
+            }
+            freeIPCount += (uint)Math.Max(0, uint.MaxValue - intervals[^1].Finish - 1);
+            return freeIPCount;
+        }
+
+        public static List<(long Start, long Finish)> GetUniqueIntervals((uint Start, uint Finish)[] data)
+        {
+            var intervals = new List<(long Start, long Finish)>();
+            int index = 0;
+            long intervalStart = data[0].Start;
+            long curMaxValue = data[0].Finish;
+            while (index < data.Length - 1)
+            {
+                // Overlapping, move to next element
+                curMaxValue = Math.Max(curMaxValue, data[index].Finish);
+                if (curMaxValue + 1 < data[index + 1].Start)
+                {
+                    intervals.Add((intervalStart, curMaxValue));
+                    curMaxValue = data[index + 1].Finish;
+                    intervalStart = data[index + 1].Start;
+                }
+                index++;
+            }
+            var last = data.Max(x => x.Finish);
+            intervals.Add((intervalStart, last));
+            return intervals;
+        }
+    }
+}
---
+++ Year2016/tests/Solutions/Day20Tests.cs
@@ -0,0 +1,82 @@
+﻿using AdventOfCode.Year2016.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Year2016.Tests.Solutions
+{
+    [TestClass]
+    public class Day20Tests
+    {
+        [TestMethod]
+        public void ConvertTests()
+        {
+            var data = new string[]
+            {
+                "5-8",
+                "0-2",
+                "4-7"
+            };
+
+            var expected = new (uint, uint)[]
+            {
+                (5,8),
+                (0,2),
+                (4,7)
+            };
+
+            CollectionAssert.AreEqual(expected, Day20.Convert(data));
+        }
+
+        [TestMethod]
+
+        public void FirstProblemTest()
+        {
+            var data = new (uint, uint)[]
+            {
+                (5,8),
+                (0,2),
+                (4,7)
+            };
+            Assert.AreEqual(3u, Day20.FirstProblem(data));
+        }
+
+        [TestMethod]
+
+        public void SecondProblemTest()
+        {
+            var data = new (uint, uint)[]
+            {
+                (0,2),
+                (4,7),
+                (5,8),
+                (11,14),
+                (16, uint.MaxValue)
+            };
+            Assert.AreEqual(4u, Day20.SecondProblem(data));
+        }
+
+        [TestMethod]
+
+        public void GetUniqueIntervalsTest()
+        {
+            var data = new (uint, uint)[]
+            {
+                (0,2),
+                (4,7),
+                (5,8),
+                (11,14),
+                (12,17),
+                (16, 22),
+                (25, uint.MaxValue - 10)
+            };
+
+            var expected = new List<(long, long)>
+            {
+                (0,2),(4,8),(11,22),(25, uint.MaxValue - 10)
+            };
+
+            CollectionAssert.AreEqual(expected, Day20.GetUniqueIntervals(data));
+        }
+
+    }
+}
