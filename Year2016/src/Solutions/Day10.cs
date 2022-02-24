96898ef3f4ec80b43952d2f541a1d2a0f00acd54
Marcel Wagner <marcel.alex.wagner@outlook.com>
2022-02-24 21:11:00 +0100

    Add day 10, 2016

---
+++ Year2016/src/Solutions/Day10.cs
@@ -0,0 +1,112 @@
+﻿using AdventOfCode.Common;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Year2016.Solutions
+{
+    public static class Day10
+    {
+        public static long FirstProblem(string[] data)
+        {
+            var bots = GenerateBots(data);
+            return ProcessInput(data, bots, (61, 17)).FoundComparison;
+        }
+
+        public static long SecondProblem(string[] data)
+        {
+            var bots = GenerateBots(data);
+            var outputs = ProcessInput(data, bots, (-1, -1)).Outputs;
+            return outputs[0].Value * outputs[1].Value * outputs[2].Value;
+        }
+
+        private static void Insert(ref this (int?, int?) tuple, int value)
+        {
+            if (tuple.Item1 == null)
+            {
+                tuple.Item1 = value;
+            }
+            else
+            {
+                tuple.Item2 = Math.Max(tuple.Item1.Value, value);
+                tuple.Item1 = Math.Min(tuple.Item1.Value, value);
+            }
+        }
+
+        public static (int?, int?)[] GenerateBots(string[] input)
+        {
+            var maxNumberMovePartOne = input.Where(x => x[0] == 'b').Select(x => int.Parse(x.Split(' ')[6])).Max();
+            var maxNumberMovePartTwo = input.Where(x => x[0] == 'b').Select(x => int.Parse(x.Split(' ')[11])).Max();
+            var botValueInstructions = input.Where(x => x[0] == 'v');
+            var maxNumberBotSetValue = botValueInstructions.Select(y => int.Parse(y.Split(" ")[5])).Max() + 1;
+
+            var size = Math.Max(maxNumberBotSetValue, Math.Max(maxNumberMovePartOne, maxNumberMovePartTwo)) + 1;
+            var values = new (int?, int?)[size];
+            Array.Fill(values, (null, null));
+            foreach (var instruction in botValueInstructions)
+            {
+                var split = instruction.Split(' ');
+                var value = int.Parse(split[1]);
+                var bot = int.Parse(split[5]);
+                values[bot].Insert(value);
+            }
+
+            return values;
+        }
+
+        public static (int FoundComparison, int?[] Outputs) ProcessInput(string[] input, (int?, int?)[] bots, (int, int) comparison)
+        {
+            var outputs = new int?[bots.Length];
+            (int?, int?) sortedComparison = (Math.Min(comparison.Item1, comparison.Item2), Math.Max(comparison.Item1, comparison.Item2));
+            Array.Fill(outputs, null);
+
+            var moveInstructions = input.Where((x) => x[0] == 'b');
+
+            var twoEntrybot = FindTwoEntryBot(bots);
+            while (twoEntrybot > -1)
+            {
+                var instruction = moveInstructions.Where((x) => int.Parse(x.Split(' ')[1]) == twoEntrybot).First();
+                var split = instruction.Split(' ');
+                if (bots[twoEntrybot] == sortedComparison)
+                {
+                    return (twoEntrybot, outputs);
+                }
+                var firstIsOutput = split[5][0] == 'o';
+                var SecondIsOutput = split[10][0] == 'o';
+                var firstIndex = int.Parse(split[6]);
+                var secondIndex = int.Parse(split[11]);
+                if (firstIsOutput)
+                {
+                    outputs[firstIndex] = bots[twoEntrybot].Item1;
+                }
+                else
+                {
+                    bots[firstIndex].Insert(bots[twoEntrybot].Item1.Value);
+                }
+                if (SecondIsOutput)
+                {
+                    outputs[secondIndex] = bots[twoEntrybot].Item2;
+                }
+                else
+                {
+                    bots[secondIndex].Insert(bots[twoEntrybot].Item2.Value);
+                }
+                bots[twoEntrybot] = (null, null);
+                twoEntrybot = FindTwoEntryBot(bots);
+            }
+
+            static int FindTwoEntryBot((int?, int?)[] curBots)
+            {
+                for (int i = 0; i < curBots.Length; i++)
+                {
+                    if (curBots[i].Item1.HasValue && curBots[i].Item2.HasValue)
+                    {
+                        return i;
+                    }
+                }
+                return -1;
+            }
+            return (-1, outputs);
+        }
+    }
+}
---
+++ Year2016/tests/Solutions/Day10Tests.cs
@@ -0,0 +1,52 @@
+﻿using AdventOfCode.Year2016.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Year2016.Tests.Solutions
+{
+    [TestClass]
+    public class Day10Tests
+    {
+
+        string[] instructions = new string[]
+        {
+            "value 5 goes to bot 2",
+            "bot 2 gives low to bot 1 and high to bot 0",
+            "value 3 goes to bot 1",
+            "bot 1 gives low to output 1 and high to bot 0",
+            "bot 0 gives low to output 2 and high to output 0",
+            "value 2 goes to bot 2",
+        };
+
+        [TestMethod]
+        public void ConvertBotTestWorks()
+        {
+            var expected = new (int?, int?)[]
+            {
+                (null,null),
+                (3,null),
+                (2,5)
+            };
+
+            CollectionAssert.AreEqual(expected, Day10.GenerateBots(instructions));
+        }
+
+        [TestMethod]
+        public void FindsComparisonCorrectlyWorks()
+        {
+            var bots = Day10.GenerateBots(instructions);
+            Assert.AreEqual(2, Day10.ProcessInput(instructions, bots, (5,2)).FoundComparison);
+        }
+
+        [TestMethod]
+        public void OutputsAreCorrect()
+        {
+            var bots = Day10.GenerateBots(instructions);
+            var expectedOutput = new int?[]
+            {
+                5,2,3
+            };
+            var actual = Day10.ProcessInput(instructions, bots, (-1, -1)).Outputs;
+            CollectionAssert.AreEqual(expectedOutput, actual);
+        }
+    }
+}
