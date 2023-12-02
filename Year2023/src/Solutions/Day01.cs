f9a3033287ec5c4066b108ecb7b3bab16bfa9709
Marcel Wagner <marcel.alex.wagner@outlook.com>
2023-12-02 13:35:42 +0100

    Minor refactoring for day 1

--- Year2023/src/Solutions/Day01.cs
+++ Year2023/src/Solutions/Day01.cs
@@ -1,29 +1,27 @@
-﻿using System.Globalization;
-
-namespace AdventOfCode.Year2023.Solutions
+﻿namespace AdventOfCode.Year2023.Solutions
 {
 	public static class Day01
 	{
-		private static Tuple<int, string>[] NUMBERS = [ 
-			Tuple.Create(1, "one"),
-			Tuple.Create(2, "two"),
-			Tuple.Create(3, "three"),
-			Tuple.Create(4, "four"),
-			Tuple.Create(5, "five"),
-			Tuple.Create(6, "six"),
-			Tuple.Create(7, "seven"),
-			Tuple.Create(8, "eight"),
-			Tuple.Create(9, "nine")
+		private static readonly (int Digit, string Name)[] NUMBERS = [
+			(1, "one"),
+			(2, "two"),
+			(3, "three"),
+			(4, "four"),
+			(5, "five"),
+			(6, "six"),
+			(7, "seven"),
+			(8, "eight"),
+			(9, "nine")
 		];
 
 		public static int FirstProblem(string[] input)
 		{
-			return input.Select(x => GetNumberForRowUsingDigits(x)).Sum();
+			return input.Select(GetNumberForRowUsingDigits).Sum();
 		}
 
 		public static int SecondProblem(string[] input)
 		{
-			return input.Select(x => GetNumberForRowUsingDigitAndName(x)).Sum();
+			return input.Select(GetNumberForRowUsingDigitAndName).Sum();
 		}
 
 		private static int GetNumberForRowUsingDigits(string input)
@@ -31,16 +29,16 @@ namespace AdventOfCode.Year2023.Solutions
 			var firstNumber = input.First(char.IsNumber);
 			var secondNumber = input.Last(char.IsNumber);
 			// Subtracting 48 since they are chars and the 0 digit in ASCII is 48.
-			return (firstNumber - 48)* 10 + secondNumber - 48;
+			return (firstNumber - 48) * 10 + secondNumber - 48;
 		}
 
 		private static int GetNumberForRowUsingDigitAndName(string input)
 		{
 			// Subtracting 48 since they are chars and the 0 digit in ASCII is 48.
-			var result = FindFirstEntry() * 10 + FindFirstEntry(true);
+			var result = FindFirstNumber() * 10 + FindFirstNumber(true);
 			return result;
 
-			int FindFirstEntry(bool fromEnd = false)
+			int FindFirstNumber(bool fromEnd = false)
 			{
 				for (int i = 0; i < input.Length; i++)
 				{
@@ -51,10 +49,10 @@ namespace AdventOfCode.Year2023.Solutions
 					}
 
 					var currentStringToEnd = input[positionInString..];
-					for (int j = 0; j < NUMBERS.Length; j++)
+					var (Digit, Name) = NUMBERS.Where(x => currentStringToEnd.StartsWith(x.Name, StringComparison.InvariantCulture)).FirstOrDefault((Digit: -1, Name: ""));
+					if (Digit != -1)
 					{
-						if (currentStringToEnd.StartsWith(NUMBERS[j].Item2))
-							return NUMBERS[j].Item1;
+						return Digit;
 					}
 				}
 				return -1;
