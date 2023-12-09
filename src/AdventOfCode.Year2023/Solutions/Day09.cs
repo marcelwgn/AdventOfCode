75cd1120a29feb3631ffa0af9fd6a1121ec5d28f
Marcel Wagner <marcel.alex.wagner@outlook.com>
2023-12-09 16:06:58 +0100

    Add year 2023, day 9

---
+++ src/AdventOfCode.Year2023/Solutions/Day09.cs
@@ -0,0 +1,62 @@
+﻿namespace AdventOfCode.Year2023.Solutions
+{
+    public class Day09
+	{
+		public static int[][] Convert(string[] data)
+		{
+			return data.Select(x => x.Split(" ").Select(int.Parse).ToArray()).ToArray();
+		}
+
+		public static long FirstProblem(int[][] data)
+		{
+			var sum = 0;
+			foreach (var item in data)
+			{
+				var derivatives = CalculateDerivationHistory(item);
+
+				var totalIncrease = derivatives.Sum(x => x[^1]);
+				sum += item[^1] + totalIncrease;
+			}
+			return sum;
+		}
+
+		public static long SecondProblem(int[][] data)
+		{
+			var sum = 0;
+			foreach(var item in data)
+			{
+				var derivatives = CalculateDerivationHistory(item);
+
+				var lastSlope = 0;
+				foreach(var derivative in derivatives)
+				{
+					lastSlope = derivative[0] - lastSlope;
+				}
+				sum += item[0] - lastSlope;
+			}
+			return sum;
+		}
+
+		private static Stack<int[]> CalculateDerivationHistory(int[] data)
+		{
+			var derivatives = new Stack<int[]>();
+			var currentDerivative = CalculateDerivatives(data);
+			while (currentDerivative.Any(x => x != 0))
+			{
+				derivatives.Push(currentDerivative);
+				currentDerivative = CalculateDerivatives(currentDerivative);
+			}
+			return derivatives;
+		}
+
+		private static int[] CalculateDerivatives(int[] data)
+		{
+			var derivatives = new int[data.Length - 1];
+			for (int i = 0; i < derivatives.Length; i++)
+			{
+				derivatives[i] = data[i + 1] - data[i];
+			}
+			return derivatives;
+		}
+	}
+}
---
+++ tests/AdventOfCode.Year2023.Tests/Solutions/Day09Tests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.Year2023.Tests.Solutions
+{
+	[TestClass]
+	public class Day09Tests
+	{
+		private readonly string[] data =
+		[
+			"0 3 6 9 12 15",
+			"1 3 6 10 15 21",
+			"10 13 16 21 30 45"
+		];
+
+
+
+		[TestMethod]
+		public void VerifyConvert()
+		{
+			var expected = new int[][]
+			{
+				[0, 3, 6, 9, 12, 15],
+				[1, 3, 6, 10, 15, 21],
+				[10, 13, 16, 21, 30, 45]
+			};
+
+			var actual = Day09.Convert(data);
+
+			Assert.AreEqual(expected.Length, actual.Length);
+			for(int i = 0; i < expected.Length; i++)
+			{
+				CollectionAssert.AreEqual(expected[i], actual[i]);
+			}
+		}
+
+		[TestMethod]
+		public void VerifyFirstProblem()
+		{
+			var parsed = Day09.Convert(data);
+
+			Assert.AreEqual(114, Day09.FirstProblem(parsed));
+		}
+
+		[TestMethod]
+		public void VerifySecondProblem()
+		{
+			var parsed = Day09.Convert(data);
+
+			Assert.AreEqual(2, Day09.SecondProblem(parsed));
+		}
+	}
+}
