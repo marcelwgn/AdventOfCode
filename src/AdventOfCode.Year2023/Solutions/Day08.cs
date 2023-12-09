c4a463c668c3c9e24c509d56831713f162b025ff
Marcel Wagner <marcel.alex.wagner@outlook.com>
2023-12-09 16:09:15 +0100

    Remove wrong LCM checker

--- src/AdventOfCode.Year2023/Solutions/Day08.cs
+++ src/AdventOfCode.Year2023/Solutions/Day08.cs
@@ -97,10 +97,6 @@ namespace AdventOfCode.Year2023.Solutions
 				{
 					throw new ArgumentException("Does not adhere to LCM rule");
 				}
-				if(loopSize % parsed.Instructions.Length != loopSize * 2 % parsed.Instructions.Length)
-				{
-					throw new ArgumentException("Does not adhere to LCM rule");
-				}
 			}
 		}
 
