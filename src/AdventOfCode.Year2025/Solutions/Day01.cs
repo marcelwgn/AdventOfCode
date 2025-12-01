10592101813709ccae5d993769cbb845479289f2
Marcel Wagner <marcel.alex.wagner@outlook.com>
2025-12-01 21:53:04 +0100

    Day 01, 2025

---
+++ src/AdventOfCode.Year2025/Solutions/Day01.cs
@@ -0,0 +1,61 @@
+﻿
+namespace AdventOfCode.Year2025.Solutions
+{
+    public static class Day01
+    {
+        public static string FirstProblem(string[] data)
+        {
+            int cRotationWasZero = 0;
+            int uCurRotation = 50;
+            foreach (var st in data)
+            {
+                bool fLeft = st[0] == 'L';
+                int uDegrees = int.Parse(st[1..]);
+                if (fLeft)
+                {
+                    uCurRotation = (uCurRotation + 100 - uDegrees) % 100;
+                }
+                else
+                {
+                    uCurRotation = (uCurRotation + uDegrees) % 100;
+                }
+                if (uCurRotation == 0)
+                {
+                    cRotationWasZero++;
+                }
+            }
+
+            return cRotationWasZero.ToString();
+        }
+
+        public static string SecondProblem(string[] data)
+        {
+            int cRotationWasZero = 0;
+            int uCurRotation = 50;
+            foreach (var st in data)
+            {
+                bool fLeft = st[0] == 'L';
+                int uDegrees = int.Parse(st[1..]);
+                bool fWasZero = uCurRotation == 0;
+                if (fLeft)
+                {
+                    uCurRotation -= (uDegrees % 100);
+                }
+                else
+                {
+                    uCurRotation += (uDegrees % 100);
+                }
+
+                cRotationWasZero += uDegrees / 100;
+
+                if (!fWasZero && (uCurRotation <= 0 || uCurRotation >= 100))
+                {
+                    cRotationWasZero++;
+                }
+                uCurRotation = ((uCurRotation % 100) + 100) % 100;
+            }
+
+            return cRotationWasZero.ToString();
+        }
+    }
+}
---
+++ tests/AdventOfCode.Year2025.Tests/Solutions/Day01Tests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Year2025.Tests.Solutions
+{
+    [TestClass]
+    public class Day01Tests
+    {
+        public static readonly string[] DATA =
+        [
+            "L68",
+            "L30",
+            "R48",
+            "L5",
+            "R60",
+            "L55",
+            "L1",
+            "L99",
+            "R14",
+            "L82",
+        ];
+
+        [TestMethod]
+        public void VerifyFirstProblem()
+        {
+            Assert.AreEqual("3", Year2025.Solutions.Day01.FirstProblem(DATA));
+        }
+
+        [TestMethod]
+        public void VerifySecondProblem()
+        {
+            Assert.AreEqual("6", Year2025.Solutions.Day01.SecondProblem(DATA));
+        }
+
+
+        [TestMethod]
+        public void VerifySecondProblemHundreds()
+        {
+            string[] data = [
+                "R200",
+                "L200",
+            ];
+            Assert.AreEqual("4", Year2025.Solutions.Day01.SecondProblem(data));
+        }
+
+
+        [TestMethod]
+        public void VerifySecondProblemHundredsCaseTwo()
+        {
+            string[] data = [
+                "R50",
+                "L200",
+            ];
+            Assert.AreEqual("3", Year2025.Solutions.Day01.SecondProblem(data));
+        }
+    }
+}
